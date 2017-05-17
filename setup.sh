31b02368bde5b2b84df2d5cf7c30418a29edbc87
cyy <cyy@linkcity.cc>
2017-05-17 09:11:38 +0800

    更新setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
+git pull
+bin/console assets:install
+
 bin/console cache:clear
 bin/console cache:clear --env=prod
 
 chmod 777 var/cache -R
 chmod 777 var/logs -R
+chmod 777 var/sessions -R
+
+bin/console doctrine:schema:update --dump-sql
+bin/console doctrine:schema:update --force
